96cbd267861ac38e67d0e7ed97216e21081bf107
anasabdulwahed <64900080+anasabdulwahed@users.noreply.github.com>
2020-05-28 17:11:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var add = document.getElementsById("page1").onclick=function
+function onclick() {
+  document.getElementById("inp1").innerHTML = Date();
+var res =[]
+for(var i =0 ; i<onclick.length;i++){
+
+res.push(onclick[i])
+
+}
+console.log('onclick'[i])
+
+}
+
+
+
+
+
+
+
+ 
+
